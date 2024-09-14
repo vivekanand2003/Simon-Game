55aa10fa873fbcfbaa9c557117d1483a1837dcc5
vivekanand2003 <vivekanand2003kumar@gmail.com>
2024-09-14 11:42:06 +0530

    update css and js files

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ document.addEventListener("keypress",function(){
     }
 })
 
-function btnFlash(btn){
+function GameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
+    },250);
+
+}
+
+// change color on user click
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
     },250);
 
 }
@@ -32,5 +41,16 @@ function levelUp(){
     console.log(randColor);
     console.log(randBtn);
 
-    btnFlash(randBtn);
+    GameFlash(randBtn);
+}
+
+function btnPress(){
+    console.log(this);
+    let btn=this;
+    userFlash(btn);
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+btn.addEventListener("click",btnPress);
 }
