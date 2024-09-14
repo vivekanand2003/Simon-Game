5292e1ccae7a14981e4f5cfd83377a5c5d158827
vivekanand2003 <vivekanand2003kumar@gmail.com>
2024-09-14 10:25:14 +0530

    add js file content

---
+++ app.js
@@ -0,0 +1,12 @@
+let gemeseq=[];
+let userseq=[];
+
+let level=0;
+let started=false;
+
+document.addEventListener("keypress",function(){
+    if (started== false){
+        console.log("Game Start");
+        started=true;
+    }
+})
